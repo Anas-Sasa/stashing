ae572a64983aa0571468bfdd45616a27b587df95
Anas-Sasa <to.works16@gmail.com>
2025-10-01 07:12:27 +0200

    St3 edited and added

--- st3.py
+++ st3.py
@@ -1,2 +1,11 @@
 print("I love stashing beacuse i want to be profissional in git!!!!")
 
+def calculate(num1, num2):
+
+    return f'{num1} x {num2} = {num2 * num1}'
+
+
+print(calculate(30, 90))
+
+
+
